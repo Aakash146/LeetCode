acc8b32fd60d84cb71c74ce0d90325c3246018ff
Aakash Singh <51032523+Aakash146@users.noreply.github.com>
2022-04-29 17:00:38 +0530

    Time: 66 ms (5.30%), Space: 52 MB (19.55%) - LeetHub

--- 524-longest-word-in-dictionary-through-deleting/524-longest-word-in-dictionary-through-deleting.java
+++ 524-longest-word-in-dictionary-through-deleting/524-longest-word-in-dictionary-through-deleting.java
@@ -1,22 +1,23 @@
 class Solution {
     public String findLongestWord(String s, List<String> dictionary) {
         String max = "";
+        Collections.sort(dictionary);
         for(int i = 0; i < dictionary.size(); i++){
             int k = 0;
             for(int j = 0 ; j < s.length(); j++){
                 if(s.charAt(j) == dictionary.get(i).charAt(k)) k++;
                 if(k == dictionary.get(i).length()){
                     if(max.length() < dictionary.get(i).length()) max = dictionary.get(i);   
-                    else if( max.length() == dictionary.get(i).length() ){
-                        for(int l = 0; l < max.length(); l++){
-                            if(max.charAt(l) > dictionary.get(i).charAt(l)){
-                                max = dictionary.get(i);   
-                                break;
-                            }
-                            else if(max.charAt(l) < dictionary.get(i).charAt(l)) break;
+//                     else if( max.length() == dictionary.get(i).length() ){
+//                         for(int l = 0; l < max.length(); l++){
+//                             if(max.charAt(l) > dictionary.get(i).charAt(l)){
+//                                 max = dictionary.get(i);   
+//                                 break;
+//                             }
+//                             else if(max.charAt(l) < dictionary.get(i).charAt(l)) break;
                             
-                        }
-                    }
+//                         }
+//                     }
                     break;
                 }
             }
