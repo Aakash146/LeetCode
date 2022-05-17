1eb6ba84512df259298345f4997617db0d0bef57
Aakash Singh <51032523+Aakash146@users.noreply.github.com>
2022-05-17 10:34:12 +0530

    Time: 5 ms (11.79%), Space: 47.8 MB (51.53%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -9,48 +9,24 @@
  * }
  */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int carry = 0;
-        ListNode sum = new ListNode(0);
+public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0; 
+        ListNode sum = new ListNode(0); // dummy head
         ListNode ans = sum;
-        
-        while(l1 != null && l2 != null){
-            sum.val = (l1.val + l2.val + carry)%10;
-            carry = (l1.val + l2.val + carry) / 10;
-            if(l1.next != null || l2.next != null){
-                sum.next = new ListNode(0);
-                sum = sum.next;
+        while (l1 != null || l2 != null || carry != 0) { // termination condition is critical
+            if (l1 != null) {
+                carry = carry + l1.val;
+                l1 = l1.next;
             }
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-        
-        if(l1 != null){
-            while(l1 != null){
-                sum.val = (l1.val + carry)%10;
-                carry = (l1.val + carry) / 10;
-                if(l1.next != null){
-                    sum.next = new ListNode(0);
-                    sum = sum.next;
-                }
-                l1 = l1.next;    
-            }    
-        }
-        else{
-            while(l2 != null){
-                sum.val = (l2.val + carry)%10;
-                carry = (l2.val + carry) / 10;
-                if(l2.next != null) {
-                    sum.next = new ListNode(0);
-                    sum = sum.next;
-                }
-                l2 = l2.next;    
+            if (l2 != null) {
+                carry = carry + l2.val;
+                l2 = l2.next;
             }
+            sum.next = new ListNode(carry % 10);
+            sum = sum.next;
+            carry = carry / 10;
         }
         
-        if(carry == 1){
-            sum.next = new ListNode(1);
-        }
-        return ans;
+        return ans.next;
     }
 }
